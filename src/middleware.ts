e2c1a60827bf963dff811b1008c305d05a20d837
ilijachrchev <ilijachrchev@gmail.com>
2025-11-07 17:33:25 +0100

    chaos^chaos^chaos

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,21 +15,27 @@
 
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublic = createRouteMatcher(['/', '/auth(.*)', '/portal(.*)', '/images(.*)']);
-const isIgnored = createRouteMatcher(['/chatbot']);
+const isPublic = createRouteMatcher([
+  "/",          
+  "/auth(.*)",   
+  "/portal(.*)",
+  "/images(.*)",
+]);
+const isIgnored = createRouteMatcher([
+  "/chatbot",     
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (isIgnored(req)) return;
-    if (isPublic(req)) {
-        await auth.protect()
-    }
+  if (isIgnored(req)) return;    
+
+  if (!isPublic(req)) {
+    await auth.protect();          
+  }
 });
 
 export const config = {
-    matcher: [
-        '/((?!.+\\.[\\w]+$|_next).*)',
-        '/',
-        '/(api|trpc)(.*)',
-    ],
-}
-
+  matcher: [
+    "/((?!.*\\..*|_next).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
