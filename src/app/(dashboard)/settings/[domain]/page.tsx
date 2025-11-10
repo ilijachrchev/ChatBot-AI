9f00fe0e350672873d207638cc9d7b946b5ab11b
ilijachrchev <ilijachrchev@gmail.com>
2025-11-10 16:03:50 +0100

    fix deploy errors

--- src/app/(dashboard)/settings/[domain]/page.tsx
+++ src/app/(dashboard)/settings/[domain]/page.tsx
@@ -5,22 +5,27 @@ import InfoBar from '@/components/infobar'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-type Props = { params: { domain: string } }
+type Props = { 
+  params: Promise<{ domain: string }>
+}
 
 const DomainSettingsPage = async ({ params }: Props) => {
-    const domain = await onGetCurrentDomainInfo(params.domain)
+    const { domain } = await params
     if (!domain) redirect('/dashboard')
+      const domainInfo = await onGetCurrentDomainInfo(domain)
+
+    if (!domainInfo) redirect('/dashboard')
   return (
     <>
         <InfoBar />
         <div className='overflow-y-auto w-full chat-window flex-1 h-0'>
             <SettingsForm
-                plan={domain.subscription?.plan!}
-                chatBot={domain.domains[0].chatBot}
-                id={domain.domains[0].id}
-                name={domain?.domains[0].name}
+                plan={domainInfo.subscription?.plan!}
+                chatBot={domainInfo.domains[0].chatBot}
+                id={domainInfo.domains[0].id}
+                name={domainInfo.domains[0].name}
             />
-            <BotTrainingForm id={domain.domains[0].id} />
+            <BotTrainingForm id={domainInfo.domains[0].id} />
         </div>
     </>
   )
