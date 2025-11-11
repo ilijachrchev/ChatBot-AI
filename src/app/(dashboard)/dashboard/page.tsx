361923f05f3043680469b7abcea0720be81caff5
ilijachrchev <ilijachrchev@gmail.com>
2025-11-11 14:37:06 +0100

    dash

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,15 @@
+import { getUserAppointments } from '@/actions/appointment'
+import { getUserBalance, getUserClients, getUserPlanInfo } from '@/actions/dashboard'
 import React from 'react'
 
 type Props = {}
 
 const page = async (props: Props) => {
 
-  // const clients = await getUserClients()
+  const clients = await getUserClients()
+  const sales = await getUserBalance()
+  const bookings = await getUserAppointments()
+  const plan = await getUserPlanInfo();
 
   return (
     <div>page</div>
