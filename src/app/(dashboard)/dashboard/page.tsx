4cc00184cba7488a6dde566db2d739dece73dab4
ilijachrchev <ilijachrchev@gmail.com>
2025-11-13 20:14:26 +0100

    dashboard

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,13 @@
 import { getUserAppointments } from '@/actions/appointment'
 import { getUserBalance, getUserClients, getUserPlanInfo, getUserTotalProductPrices, getUserTransaction } from '@/actions/dashboard'
 import DashboardCard from '@/components/dashboard/cards'
+import { PlanUsage } from '@/components/dashboard/plan-usage'
 import InfoBar from '@/components/infobar'
+import { Separator } from '@/components/ui/separator'
+import CalIcon from '@/icons/cal-icon'
 import PersonIcon from '@/icons/person-icon'
+import { TransactionsIcon } from '@/icons/transactions-icon'
+import { DollarSign } from 'lucide-react'
 import React from 'react'
 
 type Props = {}
@@ -26,6 +31,61 @@ const page = async (props: Props) => {
           title="Potential Clients"
           icon={<PersonIcon />}
         />
+        <DashboardCard
+          value={products! * clients! || 0}
+          sales
+          title="Pipline Value"
+          icon={<DollarSign />}
+        />
+        <DashboardCard
+          value={bookings || 0}
+          title="Appointments"
+          icon={<CalIcon />}
+        />
+        <DashboardCard
+          value={sales || 0}
+          sales
+          title="Total Sales"
+          icon={<DollarSign />}
+        />
+      </div>
+      <div className='w-full grid grid-cols-1 lg:grid-cols-2 py-10'>
+        <div>
+          <div>
+            <h2 className='font-bold text-2xl'>Plan Usage</h2>
+            <p className='text-sm font-light'>
+              A detailed overview of your metrcis, usage, customers and more.
+            </p>
+            <PlanUsage
+              plan={plan?.plan! || 0}
+              credits={plan?.credits || 0}
+              domains={plan?.domains || 0}
+              clients={clients || 0}
+            />
+          </div>
+          <div className='flex flex-col'>
+            <div className='w-full flex justify-between items-startmb-5'>
+              <div className='flex gap-3 items-center'>
+                <TransactionsIcon />
+                <p className='font-bold'>Recent Transactions</p>
+              </div>
+              <p className='text-sm'>See more</p>
+            </div>
+            <Separator orientation='horizontal' />
+            {transactions &&
+              transactions.data.map((transaction) => (
+                <div className='flex gap-3 w-full justify-between items-center border-b-2 py-5'
+                key={transaction.id}>
+                  <p className='font-bold'>
+                    {transaction.calculated_statement_descriptor}
+                  </p>
+                  <p className='font-bold text-xl'>
+                    ${transaction.amount / 100}
+                  </p>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </div>
     </>
--- src/components/dashboard/cards.tsx
+++ src/components/dashboard/cards.tsx
@@ -15,7 +15,7 @@ const DashboardCard = ({
 }: Props) => {
   return (
     <div className='bg-muted rounded-lg flex flex-col gap-3 pr-10 pl-10
-    py-10 md:pl-10 md:pr-20'>
+    py-10 md:pl-10 md:pr-20 border-[1px] border-border bg-stone-100 dark:bg-muted md:w-fit w-full'>
         <div className='flex gap-3'>
             {icon}
             <h2 className='font-bold text-xl'>{title}</h2>
